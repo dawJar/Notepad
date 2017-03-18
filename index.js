1c739327b8e266f849669e1777ee0147029b718a
dawJar <dawid.jaracz@gmail.com>
2017-03-19 00:46:49 +0100

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const assert = require('assert');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+// const multer = require('multer');
+const mongoose = require('mongoose');
+
+// const TestController = require('./controllers/TestController');
+
+// const upload = multer();
+const app = express();
+
+
+mongoose.Promise = global.Promise;
+
+// mongoose.connect('mongodb://localhost/app_db');
+
+// template engine
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/server/views');
+
+// set static resources
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname + '/public')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret',
+    // name: 'cookie_name',
+    // store: sessionStore, // connect-mongo session store
+    // cookie: { maxAge: 60 },
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+
+// app.use(upload.array());
+
+// example middleware for test route
+// app.use('/test', function (req, res, next) {
+//     console.log('req recieved at: ', Date.now());
+//     next();
+// });
+
+// TestController(app);
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(8080);
+console.log('you re listening to the server! 8080');
---
+++ public/entry.js
@@ -0,0 +1,6 @@
+import test from './js/test.js';
+import './sass/main.scss';
+
+console.log(test);
+console.log(test);
+console.log(test);
