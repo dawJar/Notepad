f8e7eded866d46bdb8d4fbda265604f3cc49f6f3
dawJar <dawid.jaracz@gmail.com>
2017-03-23 23:28:20 +0100

    added category text area

---
+++ public/js/components/AddNoteTextArea.jsx
@@ -0,0 +1,40 @@
+import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+
+
+class AddNoteTextArea extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        // let { addNoteCurrentNewCategory } = this.props;
+        // addNoteCurrentNewCategory(event.target.value);
+        console.log(event.target.value)
+    }
+
+    render() {
+        // console.log(this.props)
+        return (
+            <FormGroup controlId="formControlsTextarea">
+                <ControlLabel>Textarea</ControlLabel>
+                <FormControl 
+                    componentClass="textarea" 
+                    placeholder="textarea" 
+                    onChange={ this.handleChange }
+                />
+            </FormGroup>
+        );
+    }
+}
+
+AddNoteTextArea.propTypes = {
+    // value: PropTypes.string.isRequired
+};
+
+export default AddNoteTextArea;
